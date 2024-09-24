e99cce88093e6fe8d21000219777dccd12c81697
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-09-24 10:56:34 -0400

    Changed the program to display the temperature in celcius

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Francisco
+ * Created on: Sep 2024
+ * This program tells you the climate's temperature in Kelvin
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+let temperature: number
+basic.showIcon(IconNames.Happy)
+
+
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.pause(500)
+
+    temperature = input.temperature()
+    basic.showNumber(temperature)
+
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+})
